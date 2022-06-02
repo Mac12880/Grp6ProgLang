5b6ad08e5aacb956771124cec51114edbffc763e
Mac12880 <marc.juan6.mj@gmail.com>
2022-06-02 22:15:35 +0800

    Update Technical1.py

--- Technical1.py
+++ Technical1.py
@@ -1,8 +1,12 @@
-file = open('Test.txt', 'r')
+file = open("test.txt", "rt")
+#, ""
+keywords = ["if", "else", "while", ""]
+identifier = ["int", "float", "double", "string", "boolean"]
+operators = ["+", "-", "*", "/", "%", "=", "<", ">"]
+separators = ["{", "}", "[", "]"]
+file. read()
+for line in file:
+    for character in line:
+        print(character)
 
-keywords = ["", ""]
-identifier = ["", ""]
-operators = ["", ""]
-separators = ["", ""]
-
-f.close()
+file. close()
